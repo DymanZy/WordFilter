e71d9ad86d47643694a2c8fdc6bf406e79497088
dyman <dyman_zy@163.com>
2018-01-14 18:09:42 +0800

    add WordHelper file

--- app/src/main/java/com/dyman/wordfilter/MainActivity.java
+++ app/src/main/java/com/dyman/wordfilter/MainActivity.java
@@ -3,6 +3,8 @@ package com.dyman.wordfilter;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.dyman.wordfilter.bean.StringSearch;
+
 public class MainActivity extends Activity {
 
     @Override
@@ -10,4 +12,5 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
--- app/src/main/java/com/dyman/wordfilter/util/WordHelper.java
+++ app/src/main/java/com/dyman/wordfilter/util/WordHelper.java
@@ -1,5 +1,7 @@
 package com.dyman.wordfilter.util;
 
+import com.dyman.wordfilter.internal.Dict;
+
 import java.math.BigDecimal;
 
 /**
@@ -11,31 +13,59 @@ public class WordHelper {
 
     /** 半角转全角 */
     public static String toSBC(String text) {
-        return text;
+        StringBuilder sb = new StringBuilder(text);
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.toCharArray()[i];
+            if (c == 32) {
+                sb.setCharAt(i, (char)12288);
+            } else if (c < 127) {
+                sb.setCharAt(i, (char)(c + 65248));
+            }
+        }
+        return sb.toString();
     }
 
     /** 全角转半角 */
     public static String toDBC(String text) {
-        return text;
+        StringBuilder sb = new StringBuilder(text);
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.toCharArray()[i];
+            if (c == 12288) {
+                sb.setCharAt(i, (char)32);
+            } else if (c > 65280 && c < 65375) {
+                sb.setCharAt(i, (char)(c - 65248));
+            }
+        }
+        return sb.toString();
     }
 
     /** 转为繁体中文 */
     public static String toTraditionalChinese(String text) {
-        return text;
+        StringBuilder sb = new StringBuilder(text);
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.toCharArray()[i];
+            if (c >= 0x4e00 && c <= 0x9fa5) {
+                char k = Dict.Traditional.toCharArray()[c - 0x4e00];
+                if (k != c) {
+                    sb.setCharAt(i, k);
+                }
+            }
+        }
+        return sb.toString();
     }
 
     /** 转为简体中文 */
     public static String toSimplifiedChinese(String text) {
-        return text;
+        StringBuilder sb = new StringBuilder(text);
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.toCharArray()[i];
+            if (c >= 0x4e00 && c <= 0x9fa5) {
+                char k = Dict.Simplified.toCharArray()[c - 0x4e00];
+                if (k != c) {
+                    sb.setCharAt(i, k);
+                }
+            }
+        }
+        return sb.toString();
     }
-
-    /** 数字转中文大写（应用于中文金额） */
-    public static String toChineseRMB(double x) {
-        return ""+x;
-    }
-
-    public static long toNumber(String chineseString) {
-        return 0;
-    }
-
 }
